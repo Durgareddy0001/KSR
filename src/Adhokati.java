efc9951f0280af116c224055c6684c9a65449ef0
Durgareddy0001 <kdreddy0001>
2022-01-28 13:42:18 +0530

    sssss

--- src/Adhokati.java
+++ src/Adhokati.java
@@ -2,6 +2,11 @@
 public class Adhokati {
 	public static void main(String[] args) {
 		System.out.println("Done by kdr");
+		System.out.println("S I Got It");
+		System.out.println("S I Got It");
+		System.out.println("S I Got It");
+		System.out.println("S I Got It");
+		System.out.println("S I Got It");
 	}
 
 }
